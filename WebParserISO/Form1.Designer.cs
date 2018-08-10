5b46697b5eab3fbcc93ef5d5088498e4c18fa12e
steve Piller <stevepi@outlook.com>
2018-08-10 17:13:17 +0200

    Functioning but Hackish WebParser for the ISO9999 from the Rehad Website.

--- WebParserISO/Form1.Designer.cs
+++ WebParserISO/Form1.Designer.cs
@@ -28,13 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.WebtoText = new System.Windows.Forms.RichTextBox();
+            this.NodeForm = new System.Windows.Forms.TreeView();
+            this.ISOTable = new System.Windows.Forms.DataGridView();
+            this.URLadress = new System.Windows.Forms.TextBox();
+            this.WebToString = new System.Windows.Forms.Button();
+            this.LoadasTree = new System.Windows.Forms.Button();
+            this.ConvertToTable = new System.Windows.Forms.Button();
+            this.Textviewer = new System.Windows.Forms.Button();
+            this.NodeViewer = new System.Windows.Forms.Button();
+            this.Tableviewer = new System.Windows.Forms.Button();
+            this.ISOBrowser = new System.Windows.Forms.WebBrowser();
+            this.WebView = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ISOTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WebtoText
+            // 
+            this.WebtoText.Location = new System.Drawing.Point(26, 65);
+            this.WebtoText.Name = "WebtoText";
+            this.WebtoText.Size = new System.Drawing.Size(1131, 436);
+            this.WebtoText.TabIndex = 0;
+            this.WebtoText.Text = "";
+            // 
+            // NodeForm
+            // 
+            this.NodeForm.Location = new System.Drawing.Point(26, 67);
+            this.NodeForm.Name = "NodeForm";
+            this.NodeForm.Size = new System.Drawing.Size(1131, 433);
+            this.NodeForm.TabIndex = 1;
+            this.NodeForm.Visible = false;
+            // 
+            // ISOTable
+            // 
+            this.ISOTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ISOTable.Location = new System.Drawing.Point(26, 68);
+            this.ISOTable.Name = "ISOTable";
+            this.ISOTable.Size = new System.Drawing.Size(1131, 431);
+            this.ISOTable.TabIndex = 2;
+            this.ISOTable.Visible = false;
+            // 
+            // URLadress
+            // 
+            this.URLadress.Location = new System.Drawing.Point(26, 517);
+            this.URLadress.Name = "URLadress";
+            this.URLadress.Size = new System.Drawing.Size(247, 20);
+            this.URLadress.TabIndex = 3;
+            // 
+            // WebToString
+            // 
+            this.WebToString.Location = new System.Drawing.Point(146, 543);
+            this.WebToString.Name = "WebToString";
+            this.WebToString.Size = new System.Drawing.Size(199, 25);
+            this.WebToString.TabIndex = 4;
+            this.WebToString.Text = "Lade ISO in Tabelle";
+            this.WebToString.UseVisualStyleBackColor = true;
+            this.WebToString.Click += new System.EventHandler(this.WebToString_Click);
+            // 
+            // LoadasTree
+            // 
+            this.LoadasTree.Location = new System.Drawing.Point(535, 543);
+            this.LoadasTree.Name = "LoadasTree";
+            this.LoadasTree.Size = new System.Drawing.Size(199, 25);
+            this.LoadasTree.TabIndex = 5;
+            this.LoadasTree.Text = "Lade XML";
+            this.LoadasTree.UseVisualStyleBackColor = true;
+            this.LoadasTree.Click += new System.EventHandler(this.LoadasTree_Click);
+            // 
+            // ConvertToTable
+            // 
+            this.ConvertToTable.Location = new System.Drawing.Point(901, 543);
+            this.ConvertToTable.Name = "ConvertToTable";
+            this.ConvertToTable.Size = new System.Drawing.Size(199, 25);
+            this.ConvertToTable.TabIndex = 6;
+            this.ConvertToTable.Text = "Speicher ISO in XML";
+            this.ConvertToTable.UseVisualStyleBackColor = true;
+            this.ConvertToTable.Click += new System.EventHandler(this.ConvertToTable_Click);
+            // 
+            // Textviewer
+            // 
+            this.Textviewer.Location = new System.Drawing.Point(26, 12);
+            this.Textviewer.Name = "Textviewer";
+            this.Textviewer.Size = new System.Drawing.Size(46, 24);
+            this.Textviewer.TabIndex = 7;
+            this.Textviewer.Text = "Text";
+            this.Textviewer.UseVisualStyleBackColor = true;
+            this.Textviewer.Click += new System.EventHandler(this.Textviewer_Click);
+            // 
+            // NodeViewer
+            // 
+            this.NodeViewer.Location = new System.Drawing.Point(78, 12);
+            this.NodeViewer.Name = "NodeViewer";
+            this.NodeViewer.Size = new System.Drawing.Size(46, 24);
+            this.NodeViewer.TabIndex = 8;
+            this.NodeViewer.Text = "Tree";
+            this.NodeViewer.UseVisualStyleBackColor = true;
+            this.NodeViewer.Click += new System.EventHandler(this.NodeViewer_Click);
+            // 
+            // Tableviewer
+            // 
+            this.Tableviewer.Location = new System.Drawing.Point(130, 12);
+            this.Tableviewer.Name = "Tableviewer";
+            this.Tableviewer.Size = new System.Drawing.Size(46, 24);
+            this.Tableviewer.TabIndex = 9;
+            this.Tableviewer.Text = "Tabelle";
+            this.Tableviewer.UseVisualStyleBackColor = true;
+            this.Tableviewer.Click += new System.EventHandler(this.Tableviewer_Click);
+            // 
+            // ISOBrowser
+            // 
+            this.ISOBrowser.Location = new System.Drawing.Point(26, 68);
+            this.ISOBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.ISOBrowser.Name = "ISOBrowser";
+            this.ISOBrowser.Size = new System.Drawing.Size(1131, 431);
+            this.ISOBrowser.TabIndex = 10;
+            this.ISOBrowser.Visible = false;
+            // 
+            // WebView
+            // 
+            this.WebView.Location = new System.Drawing.Point(182, 12);
+            this.WebView.Name = "WebView";
+            this.WebView.Size = new System.Drawing.Size(46, 24);
+            this.WebView.TabIndex = 11;
+            this.WebView.Text = "Web";
+            this.WebView.UseVisualStyleBackColor = true;
+            this.WebView.Click += new System.EventHandler(this.WebView_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1192, 569);
+            this.Controls.Add(this.WebView);
+            this.Controls.Add(this.ISOBrowser);
+            this.Controls.Add(this.Tableviewer);
+            this.Controls.Add(this.NodeViewer);
+            this.Controls.Add(this.Textviewer);
+            this.Controls.Add(this.ConvertToTable);
+            this.Controls.Add(this.LoadasTree);
+            this.Controls.Add(this.WebToString);
+            this.Controls.Add(this.URLadress);
+            this.Controls.Add(this.ISOTable);
+            this.Controls.Add(this.NodeForm);
+            this.Controls.Add(this.WebtoText);
+            this.Name = "Form1";
+            this.Text = "Parse ISO9999 from WebPage";
+            ((System.ComponentModel.ISupportInitialize)(this.ISOTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox WebtoText;
+        private System.Windows.Forms.TreeView NodeForm;
+        private System.Windows.Forms.DataGridView ISOTable;
+        private System.Windows.Forms.TextBox URLadress;
+        private System.Windows.Forms.Button WebToString;
+        private System.Windows.Forms.Button LoadasTree;
+        private System.Windows.Forms.Button ConvertToTable;
+        private System.Windows.Forms.Button Textviewer;
+        private System.Windows.Forms.Button NodeViewer;
+        private System.Windows.Forms.Button Tableviewer;
+        private System.Windows.Forms.WebBrowser ISOBrowser;
+        private System.Windows.Forms.Button WebView;
     }
 }
 
--- WebParserISO/Form1.cs
+++ WebParserISO/Form1.cs
@@ -7,14 +7,417 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Xml;
+
 
 namespace WebParserISO
 {
     public partial class Form1 : Form
     {
+        string startpage = "https://www.rehadat-hilfsmittel.de/de/suche/index.html?connectdb=tecisolevel1&infobox=/infobox1.html&serviceCounter=1";
+        string Basisadresse = "https://www.rehadat-hilfsmittel.de/de/suche/index.html";
+        DataTable Iso9999 = new DataTable("ISO 9999");
+
         public Form1()
         {
             InitializeComponent();
+            Create_IsoTable();
+        }
+
+        private void WebToString_Click(object sender, EventArgs e)
+        {
+            Get_Categories_from_Site(startpage);
+            for (int i = 0; i < 2; i++)
+            {
+                Get_SubCategories(Iso9999);
+            }
+
+        }
+
+        private void ConvertToTable_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml("<IsoKategorien></IsoKategorien>");
+            DataRowCollection EndTabelle = Iso9999.Rows;
+            foreach (DataRow row in EndTabelle)
+            {
+                IsoKnoten(row, doc);
+            }
+
+            doc.PreserveWhitespace = true;
+            doc.Save("ISO9999.xml");
+
+        }
+
+        private void IsoKnoten(DataRow row, XmlDocument Datei)
+        {
+            XmlElement Kategorie = Datei.CreateElement("IsoKategorie");
+            Find_Parent_Cat(row,Datei).AppendChild(Kategorie);
+
+            string[] KategorieAttribute = { "Nummer", "Titel", "Beschreibung", "ICF-Codes" };
+            string[] Spalten = { " Nummer ", " Titel ", " Beschreibung ", " ICF-Codes " };
+
+            for (int i = 0; i < 4; i++)
+            {
+                XmlElement StandardElement = Datei.CreateElement(KategorieAttribute[i]);
+                StandardElement.InnerText = row[Spalten[i]].ToString();
+                Kategorie.AppendChild(StandardElement);
+            }
+            string Nummer = (string)row[" Nummer "];
+            if (Nummer.Length <= 6)
+            {
+                XmlElement newElem = Datei.CreateElement("Unterkategorien");
+                Kategorie.AppendChild(newElem);
+            }
+        }
+
+        private XmlElement Find_Parent_Cat(DataRow row, XmlDocument Datei)
+        {
+            string Kategorienummer = (string)row[" Nummer "];
+            XmlNode Parent = Datei.DocumentElement.FirstChild;
+
+            XmlElement ParentNode;
+            if (Kategorienummer.Length > 2)
+            {
+                string ParentNummer = TopLevelKat(Kategorienummer);
+                if (ParentNummer.Length > 2)
+                {
+                    ParentNummer = TopLevelKat(ParentNummer);
+                }
+
+                while (Parent.FirstChild.InnerText != ParentNummer)
+                {
+                    Parent = Parent.NextSibling;
+                }
+
+                if (Kategorienummer.Length > 5)
+                {
+                    Parent = Parent.LastChild.FirstChild;
+                    while (Parent.FirstChild.InnerText != TopLevelKat(Kategorienummer))
+                    {
+                        Parent = Parent.NextSibling;
+                    }
+
+                }
+
+                return ParentNode = (XmlElement)Parent.LastChild;
+            }
+            else
+            {
+                return Datei.DocumentElement;
+            }
+        }
+
+        private void Get_Categories_from_Site(string URL)
+        {
+            HtmlAgilityPack.HtmlDocument currentPage = HtmlStruct(URL);
+            var StartNode = currentPage.DocumentNode.SelectSingleNode("//table");
+            HtmlAgilityPack.HtmlNodeCollection Categories = StartNode.SelectNodes("./tr");
+
+            int AnzahlInKat = Categories.Count();
+            string[] FolgeLinks = new string[AnzahlInKat];
+
+            int i = 0;
+            foreach (HtmlAgilityPack.HtmlNode aktiver in Categories)
+            {
+                int k = 0;
+                HtmlAgilityPack.HtmlNodeCollection CategorieElements = aktiver.ChildNodes;
+                foreach (HtmlAgilityPack.HtmlNode SubAktiver in CategorieElements)
+                {
+                    k = k + 1;
+                    int j = 0;
+                    HtmlAgilityPack.HtmlNodeCollection WebZeile = SubAktiver.ChildNodes;
+                    foreach (HtmlAgilityPack.HtmlNode Kind in WebZeile)
+                    {
+                        j = j + 1;
+                        if (j == 1 && k == 2 && i >= 1)
+                        {
+                            //WebtoText.Text = WebtoText.Text + Kind.InnerHtml + " \r\n";
+                            Split_String_To_Entry(Kind.InnerHtml);
+                        }
+                    }
+                }
+                i = i + 1;
+                //WebtoText.Text = WebtoText.Text + "\r\n";
+            }
+        }
+
+        private void Get_SubCategories(DataTable CurrentTable)
+        {
+            DataTable Buffer = CurrentTable.Copy();
+            DataRowCollection RowCollection = Buffer.Rows;
+            int rownumber = 0;
+            foreach (DataRow row in RowCollection)
+            {
+                if ((bool)row[" Sub Gelesen "] == false)
+                {
+                    Get_Categories_from_Site(Basisadresse + (string)row[" Link "]);
+                    DataRow actualRow = Iso9999.Rows[rownumber];
+                    actualRow[" Sub Gelesen "] = true;
+                }
+                rownumber = rownumber + 1;
+            }
+        }
+
+        private void Split_String_To_Entry(string WebTableEntry)
+        {
+            DataRow row = Iso9999.NewRow();
+
+            row[" Titel "] = getBetween(WebTableEntry, "title=\"", "\">"); ;
+            row[" Link "] = Remove_Ampersands(getBetween(WebTableEntry, "href=\"", "\" title"));
+
+            string IsoNummer = getBetween(WebTableEntry, "Iso=", "\" title");
+            if (IsoNummer.Length == 0)
+            {
+                row[" Nummer "] = NummernKorrektur(IsoNummer, WebTableEntry);
+                row[" Sub Gelesen "] = true;
+            }
+            else
+            {
+                row[" Nummer "] = IsoNummer;
+            }
+            int start, end;
+            start = WebTableEntry.IndexOf("<br>", 0) + 4;
+            end = WebTableEntry.Length - start;
+            string Beschreibung = WebTableEntry.Substring(start, end);
+            if (Beschreibung.Contains("siehe)")==true)
+            {
+                Beschreibung = Beschreibung.Substring(0, WebTableEntry.IndexOf("siehe", 0 - 1));
+            }
+            row[" Beschreibung "] = Beschreibung;
+            //if(IsoNummer.Length>=4)
+            //{
+            //DataRowCollection CurrentCats = Iso9999.Rows;
+            //    DataRow InsertionRow = CurrentCats.Find(TopLevelKat(IsoNummer));
+            //    int InsertionNumber = CurrentCats.IndexOf(InsertionRow);
+            //   while (TopLevelKat((string)InsertionRow[" Nummer "]) == TopLevelKat(IsoNummer) && LowLevelKat((string)InsertionRow[" Nummer "]) < LowLevelKat(IsoNummer))
+            //    {
+            //        InsertionNumber = InsertionNumber + 1;
+            //        InsertionRow = CurrentCats[InsertionNumber];
+            //    }
+            //    CurrentCats.InsertAt(row, InsertionNumber - 1);
+            //}
+            //else
+            //{
+            Iso9999.Rows.Add(row);
+            //}
+        }
+
+        private string NummernKorrektur(string ErsterSchurf, string Quelllink)
+        {
+            string ZweiterSchurf;
+            ZweiterSchurf = getBetween(Quelllink, "nr=", "*");
+            ZweiterSchurf = ZweiterSchurf.Replace('+', '.');
+            return ZweiterSchurf;
+
+        }
+
+        public string TopLevelKat(string Nummer)
+        {
+            string TopLevel;
+            if (Nummer.Length >= 4)
+            {
+                TopLevel = Nummer.Substring(0, Nummer.Length - 3);
+                return TopLevel;
+            }
+            else
+            {
+                return Nummer;
+            }
+        }
+
+        public int LowLevelKat(string Nummer)
+        {
+            string LowLevel;
+            if (Nummer.Length >= 4)
+            {
+                LowLevel = Nummer.Substring(Nummer.Length - 2, 2);
+                int LowLevelKat = Int32.Parse(LowLevel);
+                return LowLevelKat;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private string Remove_Ampersands(string link)
+        {
+            while (link.Contains("amp;") == true)
+            {
+                int start = link.IndexOf("amp;", 0);
+                link = link.Remove(start, 4);
+
+            }
+            return link;
+        }
+
+        private void Create_IsoTable()
+        {
+            DataColumn column;
+
+            column = new DataColumn
+            {
+                DataType = System.Type.GetType("System.String"),
+                ColumnName = " Nummer "
+
+            };
+            Iso9999.Columns.Add(column);
+
+            column = new DataColumn
+            {
+                DataType = System.Type.GetType("System.String"),
+                ColumnName = " Titel "
+
+            };
+            Iso9999.Columns.Add(column);
+
+            column = new DataColumn
+            {
+                DataType = System.Type.GetType("System.String"),
+                ColumnName = " Beschreibung "
+
+            };
+            Iso9999.Columns.Add(column);
+
+            column = new DataColumn
+            {
+                DataType = System.Type.GetType("System.String"),
+                ColumnName = " ICF-Codes ",
+                DefaultValue = ""
+
+            };
+            Iso9999.Columns.Add(column);
+
+            column = new DataColumn
+            {
+                DataType = System.Type.GetType("System.String"),
+                ColumnName = " Link "
+
+            };
+            Iso9999.Columns.Add(column);
+
+            column = new DataColumn("BoolProperty", typeof(bool))
+            {
+                DefaultValue = false,
+                ColumnName = " Sub Gelesen "
+
+            };
+            Iso9999.Columns.Add(column);
+
+            DataColumn[] PrimaryKeyColumns = new DataColumn[1];
+            PrimaryKeyColumns[0] = Iso9999.Columns[" Nummer "];
+            Iso9999.PrimaryKey = PrimaryKeyColumns;
+
+            ISOTable.DataSource = Iso9999;
+
+
+        }
+
+        private string WebToText(string URL)
+        {
+
+            WebClient client = new WebClient
+            {
+                Encoding = Encoding.UTF8
+            };
+            string PageText = client.DownloadString(URL);
+            PageText.Replace("&nbsp;", "");
+            PageText.Replace("K&ouml;ln", "Köln");
+            //WebClient client = new WebClient();
+            //{
+            //    var htmlData = client.DownloadData(URL);
+            //    PageText = Encoding.UTF8.GetString(htmlData);
+
+            //}
+            return PageText;
+        }
+
+        private HtmlAgilityPack.HtmlDocument HtmlStruct(string URL)
+        {
+            HtmlAgilityPack.HtmlWeb Webpage = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument htmldoc = Webpage.Load(URL);
+
+            return htmldoc;
+
+        }
+
+        private XmlDocument TextToTree(string PageText)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(PageText);
+
+            return doc;
+
+        }
+
+        private void Textviewer_Click(object sender, EventArgs e)
+        {
+            WebtoText.Visible = true;
+            NodeForm.Visible = false;
+            ISOTable.Visible = false;
+            ISOBrowser.Visible = false;
+        }
+
+        private void NodeViewer_Click(object sender, EventArgs e)
+        {
+            WebtoText.Visible = false;
+            NodeForm.Visible = true;
+            ISOTable.Visible = false;
+            ISOBrowser.Visible = false;
+        }
+
+        private void Tableviewer_Click(object sender, EventArgs e)
+        {
+            WebtoText.Visible = false;
+            NodeForm.Visible = false;
+            ISOTable.Visible = true;
+            ISOBrowser.Visible = false;
+        }
+
+        private void WebView_Click(object sender, EventArgs e)
+        {
+            WebtoText.Visible = false;
+            NodeForm.Visible = false;
+            ISOTable.Visible = false;
+            ISOBrowser.Visible = true;
+        }
+
+        private void LoadasTree_Click(object sender, EventArgs e)
+        {
+
+            XmlDocument dom = new XmlDocument();
+            dom.Load(Application.StartupPath + "\\ISO9999.Xml");
+            WebtoText.Text = dom.DocumentElement.Name+"\r\n\n";
+            WebtoText.Text = WebtoText.Text + dom.DocumentElement.FirstChild.Name + "\r\n\n";
+            WebtoText.Text = WebtoText.Text + dom.DocumentElement.FirstChild.FirstChild.Name + "\r\n\n";
+            WebtoText.Text = WebtoText.Text + dom.DocumentElement.FirstChild.FirstChild.InnerText + "\r\n\n";
+            WebtoText.Text = WebtoText.Text + dom.DocumentElement.FirstChild.LastChild.Name + "\r\n\n";
+            WebtoText.Text = WebtoText.Text + dom.DocumentElement.FirstChild.LastChild.FirstChild.Name + "\r\n\n";
+            WebtoText.Text = WebtoText.Text + dom.DocumentElement.FirstChild.LastChild.FirstChild.FirstChild.InnerText + "\r\n\n";
+            WebtoText.Text = WebtoText.Text + dom.DocumentElement.FirstChild.LastChild.FirstChild.LastChild.Name + "\r\n\n";
+
+            XmlNode Parent = dom.DocumentElement.FirstChild.LastChild.FirstChild.FirstChild;
+            WebtoText.Text = WebtoText.Text + Parent.Name;
         }
     }
 }
